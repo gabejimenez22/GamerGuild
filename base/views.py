59200f96daf39e4452e68c02884ace4d97d45928
gabe <gabejimenez22@outlook.com>
2024-10-01 19:42:08 -0400

    User Logout

--- base/views.py
+++ base/views.py
@@ -4,6 +4,7 @@ from .models import Room, Topic
 from .forms import RoomForm
 from django.contrib.auth.models import User
 from django.contrib import messages
+from django.contrib. auth import authenticate, login, logout
 
 
 def loginPage(request):
@@ -15,6 +16,14 @@ def loginPage(request):
             user = user.objects.get(username=username)
         except:
             messages.error(request,'user does not exist')
+
+            user = authenticate(request,username=username, password= password )
+            
+            if user is not None:
+                login(request,user)
+                return redirect('home')
+            else:
+                 messages.error(request,'username or password does not exist')
     context = {}
     return render(request, 'base/login_register.html', context)
 
