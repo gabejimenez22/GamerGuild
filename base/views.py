2bdc0ba38086e39f34aa57eb7966c0d60b2fa481
gabe <gabejimenez22@outlook.com>
2024-10-16 18:34:18 -0400

    user Profile Page

--- base/views.py
+++ base/views.py
@@ -64,13 +64,14 @@ def home(request):
 
     topics = Topic.objects.all()    
     room_count = rooms.count()
-    context = {'rooms': rooms, 'topics': topics, 'room_count':  room_count}
+    room_messages = Messages.objects.filter(Q(room__topic__name__icontains=q))
+    context = {'rooms': rooms, 'topics': topics, 'room_count':  room_count, 'room_messages':  room_count}
     return render(request, 'base/home.html',context)
 
 
 def room(request,pk):
     room = Room.objects.get(id=pk)
-    room_messages = room.message_set.all().order_by('-created')
+    room_messages = room.message_set.all()
     participants = room.participants.all()
 
 
